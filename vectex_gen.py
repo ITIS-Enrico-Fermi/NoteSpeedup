e8b75fc3c479fc86165bbb1b623c7cecd56fd82c
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2023-03-09 12:48:16 +0100

    create script for vector generator

--- vectex_gen.py
+++ vectex_gen.py
@@ -4,10 +4,10 @@ import argparse
 
 def genericSizeMatrix(elementName: str, maxRowIndex: str, maxColIndex: str) -> str:
 	matrixContent = \
-		f"{elementName}_{{11}} & {elementName}_{{12}} ... & {elementName}_{{1{maxColIndex}}} \\\\ " \
-		f"{elementName}_{{21}} & {elementName}_{{22}} ... & {elementName}_{{2{maxColIndex}}} \\\\ " \
+		f"a_{{11}} & a_{{12}} ... & a_{{1{maxColIndex}}} \\\\ " \
+		f"a_{{21}} & a_{{22}} ... & a_{{2{maxColIndex}}} \\\\ " \
 		"\\vdots & \\vdots & \\vdots \\\\ " \
-		f"{elementName}_{{{maxRowIndex}1}} & {elementName}_{{{maxRowIndex}2}} ... & {elementName}_{{{maxRowIndex}{maxColIndex}}} \\\\"
+		f"a_{{{maxRowIndex}1}} & a_{{{maxRowIndex}2}} ... & a_{{{maxRowIndex}{maxColIndex}}} \\\\"
 
 	return matrixContent
 
@@ -16,7 +16,7 @@ def concreteSizeMatrix(elementName: str, rowsNumber: int, colsNumber: int) -> st
 
 	for r in range(1, rowsNumber+1):
 		for c in range(1, colsNumber+1):
-			matrixContent += f"{elementName}_{{{r}{c}}}"
+			matrixContent += f"a_{{{r}{c}}}"
 			if c != colsNumber:
 				matrixContent += " & "
 			else:
@@ -38,9 +38,8 @@ def main(genericMatrix: bool, elementName: str, rowsNumber: str, colsNumber: str
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
-	parser.add_argument("-g", "--generic", help="Generate generic size matrix", default=False, action="store_true")
-	parser.add_argument("-r", "--rows", help="Matrix rows number", type=str, default=None, required=True)
-	parser.add_argument("-c", "--columns", help="Matrix columns number", type=str, default=None, required=True)
+	parser.add_argument("-g", "--generic", help="Generate generic size vector", default=False, action="store_true")
+	parser.add_argument("-s", "--size", help="Vector size", type=str, default=None, required=True)
 	parser.add_argument("-e", "--element", help="Element name", type=str, default="a", required=False)
 	args = parser.parse_args()
 	main(args.generic, args.element, args.rows, args.columns)
