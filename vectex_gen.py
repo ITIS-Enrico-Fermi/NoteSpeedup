71438fcdc2b901f0e1daab9b10de155f3be9b91e
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2023-03-10 15:23:54 +0100

    working vector generator

--- vectex_gen.py
+++ vectex_gen.py
@@ -1,28 +1,34 @@
 #!/bin/python3
 
 import argparse
+from enum import Enum
 
-def genericSizeVector(elementName: str, size: str) -> str:
-	vectorContent = f"{elementName}_{{1}} & {elementName}_{{2}} ... & {elementName}_{{{size}}} "
+class VectorDirection(Enum):
+	VERTICAL = "\\\\"
+	HORIZONTAL = "&"
+
+def genericSizeVector(elementName: str, size: str, delimiter: str) -> str:
+	vectorContent = f"{elementName}_{{1}} {delimiter} {elementName}_{{2}} ... {delimiter} {elementName}_{{{size}}} "
 
 	return vectorContent
 
-def concreteSizeVector(elementName: str, size: int) -> str:
+def concreteSizeVector(elementName: str, size: int, delimiter: str) -> str:
 	vectorContent = ""
 
 	for i in range(1, size+1):
 		vectorContent += f"{elementName}_{{{i}}}"
 		if i != size:
-			vectorContent += " & "
+			vectorContent += f" {delimiter} "
 	
 	return vectorContent
 
-def main(elementName: str, size: str):
+def main(elementName: str, size: str, direction: VectorDirection):
 	unformattedDelimiters = "\\begin{{pmatrix}} {} \end{{pmatrix}}"
+	
 	vectorContent = \
-		concreteSizeVector(elementName, int(size))	 if size.isnumeric() \
-		else genericSizeVector(elementName, size)
-
+		concreteSizeVector(elementName, int(size), direction.value)	 if size.isnumeric() \
+		else genericSizeVector(elementName, size, direction.value)
+	
 	print(
 		unformattedDelimiters.format(
 			vectorContent
@@ -35,5 +41,8 @@ if __name__ == "__main__":
 	)
 	parser.add_argument("-s", "--size", help="Vector size. Can be either an integer or an index (last index of a generic size vector)", type=str, default=None, required=True)
 	parser.add_argument("-e", "--element", help="Element name", type=str, default="a", required=False)
+	selectDirection = parser.add_mutually_exclusive_group(required=True)
+	selectDirection.add_argument("--vertical", help="Generate a vertical vector", action="store_true")
+	selectDirection.add_argument("--horizontal", help="Generate an horizontal vector", action="store_true")
 	args = parser.parse_args()
-	main(args.element, args.size)
+	main(args.element, args.size, VectorDirection.VERTICAL if args.vertical else VectorDirection.HORIZONTAL)
