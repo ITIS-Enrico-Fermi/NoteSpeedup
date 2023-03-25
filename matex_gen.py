e548bf7090377a075fd39274e87129d634575008
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2023-03-25 21:23:35 +0100

    restructure cli arguments

--- matex_gen.py
+++ matex_gen.py
@@ -14,10 +14,10 @@ def diagoOnly(elementName: str, maxRowIndex: str, maxColIndex: str) -> str:
 
 def genericSizeMatrix(elementName: str, maxRowIndex: str, maxColIndex: str) -> str:
 	matrixContent = \
-		f"{elementName}_{{11}} & {elementName}_{{12}} & ... & {elementName}_{{1{maxColIndex}}} \\\\ " \
-		f"{elementName}_{{21}} & {elementName}_{{22}} & ... & {elementName}_{{2{maxColIndex}}} \\\\ " \
+		f"{elementName}_{{11}} & {elementName}_{{12}} & \dots & {elementName}_{{1{maxColIndex}}} \\\\ " \
+		f"{elementName}_{{21}} & {elementName}_{{22}} & \dots & {elementName}_{{2{maxColIndex}}} \\\\ " \
 		"\\vdots & \\vdots & \\ddots & \\vdots \\\\ " \
-		f"{elementName}_{{{maxRowIndex}1}} & {elementName}_{{{maxRowIndex}2}} & ... & {elementName}_{{{maxRowIndex}{maxColIndex}}} \\\\"
+		f"{elementName}_{{{maxRowIndex}1}} & {elementName}_{{{maxRowIndex}2}} & \dots & {elementName}_{{{maxRowIndex}{maxColIndex}}} \\\\"
 
 	return matrixContent
 
@@ -54,11 +54,26 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(
 		description = "Command line tool for LaTeX matrix generation"
 	)
-	parser.add_argument("-g", "--generic", help="Generate generic size matrix", default=False, action="store_true")
-	parser.add_argument("-d", "--diagonal", help="Generate generic diagonal matrix", default=False, action="store_true")
-	parser.add_argument("-r", "--rows", help="Matrix rows number", type=str, default=None, required=True)
-	parser.add_argument("-c", "--columns", help="Matrix columns number", type=str, default=None, required=True)
-	parser.add_argument("-e", "--element", help="Element name", type=str, default="a", required=False)
-	args = parser.parse_args()
-	main(args.generic, args.element, args.rows, args.columns, args.diagonal)
 
+	ElementsType = parser.add_mutually_exclusive_group(required=True)
+	ElementsType.add_argument("-g", "--generic", help="Generate a matrix full of coefficients (without explicit values)", default=False, action="store_true")
+	ElementsType.add_argument("-n", "--numerical", help="Generate a numerical matrix (full of scalar values)", default=False, action="store_true")
+	
+	Compression = parser.add_mutually_exclusive_group(required=True)
+	Compression.add_argument("-p", "--compact", help="Generate a compact matrix by omitting central rows and columns", default=False, action="store_true")
+	Compression.add_argument("-e", "--expanded", help="Generate a full-size matrix", default=False, action="store_true")
+	
+	parser.add_argument("-s", "--symbol", help="Specify a generic element name or symbol, usually a letter", type=str, default="c", required=False)
+
+	parser.add_argument("-r", "--rows", help="Matrix rows number. Can be a letter.", type=str, default=None, required=True)
+	parser.add_argument("-c", "--columns", help="Matrix columns number. Can be a letter.", type=str, default=None, required=True)
+	
+	matrixShapeSubparser = parser.add_subparsers(help="Define matrix shape", required=True)
+	matrixShapeFull = matrixShapeSubparser.add_parser("full", help="Generate full (complete) matrix")
+	matrixShapeEye = matrixShapeSubparser.add_parser("eye", help="Generate identity matrix")
+	matrixShapeDiag = matrixShapeSubparser.add_parser("diag", help="Generate diagonal matrix")
+	matrixShapeTriu = matrixShapeSubparser.add_parser("triu", help="Generate upper triangular matrix")
+	matrixShapeTril = matrixShapeSubparser.add_parser("tril", help="Generate lower triangular matrix")	
+
+	args = parser.parse_args()
+	# main(args.generic, args.element, args.rows, args.columns, args.diagonal)
